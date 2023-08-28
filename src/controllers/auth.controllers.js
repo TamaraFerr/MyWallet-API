f933538eb6833cdf90ecfe119ea3d76abeca53b2
TamaraFerr <tamarablacksmith@hotmail.com>
2023-08-27 23:10:45 -0300

    feat: users collections

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import { db } from "../database/database.connection.js"
 import bcrypt from "bcrypt"
+import {v4 as uuid} from "uuid"
 
 export async function cadastro(req, res) {
     const {name, email, password} = req.body
@@ -13,6 +14,41 @@ export async function cadastro(req, res) {
         await db.collection("usuarios").insertOne({name, email, password: hash})
         res.sendStatus(201)
 
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+}
+
+export async function login(req, res) {
+    const {email, password} = req.body
+
+    try{
+        const usuario = await db.collection("usuarios").findOne({email})
+        if(!usuario) return res.status(401).send("E-mail não existe.")
+
+        const senhaCorreta = bcrypt.compareSync(password, usuario.password)
+        if(!senhaCorreta) return res.status(401).send("Senha incorreta.")
+
+        const token = uuid()
+        await db.collection("sessoes").insertOne({token, usuarioId: usuario._id})
+        res.send({token, usuarioName: usuario.name})
+
+    } catch (err){
+        res.status(500).send(err.message)
+    }
+}
+
+export async function logout(req, res) {
+    const {authorization} = req.headers
+    const token = authorization?.replace("Bearer", "")
+    if(!token) return res.sendStatus(401)
+
+    try{
+        const sessoes = await db.collection("sessoes").findOne({token})
+        if(!sessoes) return res.sendStatus(401)
+
+        await db.collection("sessoes").deleteOne({token})
+        res.sendStatus(200)
     } catch (err){
         res.status(500).send(err.message)
     }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { validateSchema } from "../middlewares/validateSchema.middleware.js";
-import { cadastroSchema } from "../schemas/auth.schemas.js";
-import { cadastro } from "../controllers/auth.controllers.js";
+import { cadastroSchema, loginSchema } from "../schemas/auth.schemas.js";
+import { cadastro, login, logout } from "../controllers/auth.controllers.js";
 
 const authRouter = Router()
 
 authRouter.post("/cadastro", validateSchema(cadastroSchema), cadastro)
-authRouter.post("/login")
-authRouter.post("/logout")
+authRouter.post("/login", validateSchema(loginSchema), login)
+authRouter.post("/logout", logout)
 
 export default authRouter
